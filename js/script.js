cc55936719e95ebfc35ef7b32ebb6e44acf374ae
ayan120700 <yusifliayan00@gmail.com>
2020-02-02 14:25:17 +0400

    02.02-js

---
+++ js/script.js
@@ -0,0 +1,107 @@
+
+
+// let wgt = document.getElementsByClassName('weight')[0];
+// let prc = document.getElementsByClassName('price')[0];
+// wgt.style.height = '30px';
+// prc.style.height = '30px';
+
+
+
+
+
+// wgt.addEventListener('keyup' , function(){
+ 
+//     if(wgt.value <= 0.25){
+//         let result = wgt.value * 2.50
+//         prc.value = result + '$'
+//     }else if(wgt.value > 0.25 && wgt.value <= 0.5){
+//         let result = wgt.value * 3.50;
+//         prc.value = result + "$"
+//     }else if(wgt.value > 0.5 && wgt.value <= 0.75){
+//         let result = wgt.value * 4.50;
+//         prc.value = result + "$"
+//     }else if(wgt.value > 0.75 && wgt.value <= 1.0){
+//         let result = wgt.value * 5.50;
+//         prc.value = result + "$"
+//     }else if(wgt.value > 2 && wgt.value <= 5){
+//         let result = wgt.value * 5.00;
+//         prc.value = result + "$"
+//     }else if(wgt.value > 5 && wgt.value < 10){
+//         let result = wgt.value * 4.50;
+//         prc.value = result + "$"
+//     }else if(wgt.value > 10){
+//         let result = wgt.value * 4.00;
+//         prc.value = result + "$"
+//     }
+
+// });
+
+
+
+// let login = document.getElementsByClassName('login')[0];
+// let password = document.getElementsByClassName('passw')[0];
+// let button = document.getElementsByClassName("button")[0];
+
+// button.addEventListener('click', function(){
+//     let password = document.getElementsByClassName('passw')[0];
+
+//     if(login.value != "admin"){
+//         login.style.backgroundColor = 'red';
+//         password.style.backgroundColor = 'white';
+//     }else
+//     login.style.backgroundColor = 'lightgreen'
+//     password.style.backgroundColor = 'lightgreen'
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+// let month = document.querySelectorAll('.month');
+// let range = document.querySelectorAll('.rng');
+// let result = document.querySelectorAll('.result');
+
+
+// range[0].addEventListener('input', function(){
+//     let num0 = Number(range[0].value)
+//     let num1= Number(range[1].value)
+//     let num2 = Number (range[2].value)
+//     month[0].value = range[0].value;
+//     result[0].value = num1 + (num1 * num2 / 100)
+//     month[1].value = range[1].value;
+//     result[1].value = (num1 * num2 / 100)
+//     month[2].value = range[2].value;
+//     result[2].value = (num1 + (num1 * num2 / 100)) / num0
+// })
+// range[1].addEventListener('input', function(){
+//     let num0 = Number(range[0].value)
+//     let num1= Number(range[1].value)
+//     let num2 = Number (range[2].value)
+//     month[0].value = range[0].value;
+//     result[0].value = num1 + (num1 * num2 / 100)
+//     month[1].value = range[1].value;
+//     result[1].value = (num1 * num2 / 100)
+//     month[2].value = range[2].value;
+//     result[2].value = (num1 + (num1 * num2 / 100)) / num0
+// })
+// range[2].addEventListener('input', function(){
+//     let num0 = Number(range[0].value)
+//     let num1= Number(range[1].value)
+//     let num2 = Number (range[2].value)
+//     let num3 = Number(result[0].value)
+//     month[0].value = range[0].value;
+//     result[0].value = num1 + (num1 * num2 / 100)
+//     month[1].value = range[1].value;
+//     result[1].value = (num1 * num2 / 100)
+//     month[2].value = range[2].value;
+//     result[2].value = (num1 + (num1 * num2 / 100)) / num0
+// })
+
